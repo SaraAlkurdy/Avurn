42aeb6076e3c4dd2bbe91310bf68a0d5548f0033
Sara Alkurdy <sara.alkurdy@gmail.com>
2022-02-02 16:20:48 +0200

    Changes.

--- src/app/landing-page/components/_Portfolio/number-speaks/number-speaks.component.ts
+++ src/app/landing-page/components/_Portfolio/number-speaks/number-speaks.component.ts
@@ -13,22 +13,42 @@ export class NumberSpeaksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
-    const el1 = document.querySelector('.counter');
-    const el2 = document.querySelector('.counter2');
-    const el3 = document.querySelector('.counter3');
-    console.log(el1);
-    counterUp(el1, {
-      duration: 10000,
-      delay: 20,
-    });
-    counterUp(el2, {
-      duration: 10000,
-      delay: 20,
-    });
-    counterUp(el3, {
-      duration: 10000,
-      delay: 20,
-    });
+    this.initAnimatedCounts();
   }
+
+ initAnimatedCounts = () => {
+  const ease = (n) => {
+    return --n * n * n + 1;
+  };
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // Once this element is in view and starts animating, remove the observer,
+        // because it should only animate once per page load.
+        observer.unobserve(entry.target);
+        const countToString = entry.target.getAttribute('data-countTo');
+        const countTo = parseFloat(countToString);
+        const duration = parseFloat(entry.target.getAttribute('data-animateDuration'));
+        const countToParts = countToString.split('.');
+        const precision = countToParts.length === 2 ? countToParts[1].length : 0;
+        const startTime = performance.now();
+        const step = (currentTime) => {
+          const progress = Math.min(ease((currentTime  - startTime) / duration), 1);
+          entry.target.textContent = (progress * countTo).toFixed(precision);
+          if (progress < 1) {
+            animationFrame = window.requestAnimationFrame(step);
+          } else {
+            window.cancelAnimationFrame(animationFrame);
+          }
+        };
+        let animationFrame = window.requestAnimationFrame(step);
+      }
+    });
+  });
+  document.querySelectorAll('[data-animateDuration]').forEach((target) => {
+    target.setAttribute('data-countTo', target.textContent);
+    target.textContent = '0';
+    observer.observe(target);
+  });
+};
 }
